77ff065c062aa0d9a7f16d52bf3cf93efd283426
labsfiap <labsfiap@PA501MICRO23.fiap.com.br>
2024-10-21 22:22:53 -0300

    Adicionando DML

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_endereco (
+    rua,
+    numero,
+    cep,
+    bairro,
+    cidade,
+    pais,
+    complemento
+) VALUES (
+    'AVENIDA DO ESTADO',
+    900,
+    '27614533',
+    'CENTRO',
+    'GUAIANAZES',
+    'BRASIL',
+    'CASA'
+);
